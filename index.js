cbccebc53f29ada7fada45d57828a0695799b611
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-03-19 01:47:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,16 +62,7 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/employee", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}).then(() => {
-  console.log("MongoDB connected");
-}).catch(err => {
-  console.error("MongoDB connection error:", err);
-  process.exit(1); // Exit with failure
-});
+ mongoose.connect("mongodb+srv://kushsahu144114:Q1aZGaVCJ9kAAje6@cluster0.peog2iz.mongodb.net/");
 
 app.get("/", (req, res) => {
   res.json("hello");
