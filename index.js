22775605f7a99a029d92ed3555f69c79bb6a0ee0
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-03-19 00:37:46 +0530

    Update index.js
    cors

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
-const express = require("express");
-const mongoose = require('mongoose');
-const cors = require("cors");
-const EmployeeModel = require('./models/Employees');
+const express=require("express")
+const mongoose=require('mongoose')
+const cors=require("cors")
+const EmployeeModel=require('./models/Employees')
+const app=express()
+const PORT=process.env.PORT || 3001 
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(cors(
+  {
+    origin: ["https://"],
+    method:["POST","GET"],
+    credentials:true
+  }
+));
+app.use(express.json())
+// mongoose.connect("mongodb://127.0.0.1:27017/employee");
+mongoose.connect("mongodb+srv://kushsahu144114:Q1aZGaVCJ9kAAje6@cluster0.peog2iz.mongodb.net/");
+// 
 
-app.use(express.json());
-
-// CORS configuration
-const corsOptions = {
-  origin: 'https://65f75fd5a4d565d6126a2f57--whimsical-daifuku-c61cd3.netlify.app', // Remove trailing slash
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Add allowed headers
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
-
-// MongoDB connection
-mongoose.connect("mongodb+srv://kushsahu144114:Q1aZGaVCJ9kAAje6@cluster0.peog2iz.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Routes
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  EmployeeModel.findOne({ email: email })
-    .then(user => {
-      if (user) {
-        if (user.password === password) {
-          res.json("Success");
-        } else {
-          res.json("the pass is incorrect");
+app.post("/login",(req,res)=>{
+    const {email,password}=req.body;
+    EmployeeModel.findOne({email:email})
+    .then(user=>{
+        if(user){
+            if(user.password===password){
+                res.json("Success")
+            }else {
+                res.json("the pass is incorrect")
+            }
+        }else{
+            res.json("No record exist")
         }
-      } else {
-        res.json("No record exist");
-      }
-    });
-});
+    })
+})
 
-app.post('/register', (req, res) => {
-  EmployeeModel.create(req.body)
-    .then(employees => res.json(employees))
-    .catch(err => res.json(err));
-});
-
-// Server listening
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-});
+app.post('/register',(req,res)=>{
+   EmployeeModel.create(req.body)
+   .then(employees=>res.json(employees))
+   .catch(err=>res.json(err))
+})
+app.listen(PORT,()=>{
+    console.log("server is running")
+})
