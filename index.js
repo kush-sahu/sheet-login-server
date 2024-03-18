aa802b910976b1d0a8ca4f1f0cde7e2fcdb3b670
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-03-19 02:52:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const PORT=process.env.PORT || 3001
 
 app.use(cors(
   {
-    origin: ["https://65f89a2865bdaa1a01b4db75--unique-gaufre-b57be4.netlify.app"],
-    method:["POST","GET"],
+    origin: [""],
+    methods:["POST","GET"],
     credentials:true
   }
 ));
