01d2d199e24b7c455f8a1af0237e11b1d7132f48
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-03-19 03:23:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const cors=require("cors")
 const EmployeeModel=require('./models/Employees')
 const app=express()
 const PORT=process.env.PORT || 3001 
-
-app.use(cors(
-  {
-    origin: ["https://65f89a2865bdaa1a01b4db75--unique-gaufre-b57be4.netlify.app"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-));
 app.use(express.json())
+app.use(cors(
+  // {
+  //   origin: ["https://65f89a2865bdaa1a01b4db75--unique-gaufre-b57be4.netlify.app"],
+  //   methods:["POST","GET"],
+  //   credentials:true
+  // }
+));
+
 // mongoose.connect("mongodb://127.0.0.1:27017/employee");
 mongoose.connect("mongodb+srv://kushsahu144114:Q1aZGaVCJ9kAAje6@cluster0.peog2iz.mongodb.net/");
 // 
